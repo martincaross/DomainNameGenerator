9cf1062f16679d0a4b7db30722074b914b125a1e
Martín <martinjcaro@gmail.com>
2025-02-26 21:09:09 +0100

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import "bootstrap";
+import "./style.css";
+
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+window.onload = function() {
+  //write your code here
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'when I was sleeping', 'during my lunch', 'while I was praying'];
+
+  function ExcuseGenerator(){
+    let randomWho = who[Math.floor(Math.random() * who.length)];
+    let randomAction = action[Math.floor(Math.random() * action.length)];
+    let randomWhat = what[Math.floor(Math.random() * what.length)];
+    let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+    let excusa = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+    document.getElementById("excuse").innerHTML= excusa
+  }
+  
+  ExcuseGenerator();
+  
+};
