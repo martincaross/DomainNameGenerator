33a603939585df84b2ccb2a7c2e6fcb392a2ac9d
Martín <martinjcaro@gmail.com>
2025-03-03 18:05:52 +0000

    second commit

--- src/app.js
+++ src/app.js
@@ -1,25 +1,41 @@
 import "bootstrap";
 import "./style.css";
 
+let pronoun = ['the', 'our', 'my', 'your'];
+let adj = ['great', 'big', 'fast', 'small'];
+let noun = ['jogger', 'racoon', 'developer', 'designer'];
+let ext = ['.com', '.es', '.net', '.io', '.dev'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  let pronoun = ['the', 'our'];
-  let adj = ['great', 'big'];
-  let noun = ['jogger', 'racoon'];
-
-  function DomainGenerator(){
-    let randPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
-    let randomAdj = adj[Math.floor(Math.random() * adj.length)];
-    let randomNoun = noun[Math.floor(Math.random() * noun.length)];
+window.onload = function () {
+  function generateDomains() {
+    let domainList = ""; 
     
-    let dom = `${randPronoun}${randomAdj}${randomNoun}.com`;
-    document.getElementById("dom").innerHTML= dom
+    for (let i = 0; i < 5; i++) { 
+      let randPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
+      let randomAdj = adj[Math.floor(Math.random() * adj.length)];
+      let randomNoun = noun[Math.floor(Math.random() * noun.length)];
+      let randomExt = ext[Math.floor(Math.random() * ext.length)];
+      
+      let domain = `${randPronoun}${randomAdj}${randomNoun}${randomExt}`;
+      domainList += `<p class='alert alert-warning'>${domain}</p>`;
+    }
+    
+    document.getElementById("domainList").innerHTML = domainList;
   }
-  
-  DomainGenerator();
-  
+
+  generateDomains();
+
+  document.getElementById("generateDomain").addEventListener("click", generateDomains);
+
+  document.getElementById("darkModeToggle").addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+    let icon = document.querySelector("#darkModeToggle i");
+    if (document.body.classList.contains("dark-mode")) {
+      icon.classList.remove("fa-moon");
+      icon.classList.add("fa-sun");
+    } else {
+      icon.classList.remove("fa-sun");
+      icon.classList.add("fa-moon");
+    }
+  });
 };
